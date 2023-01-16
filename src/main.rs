96adb3d4c6660e8f7cbc5ef6da4b527a9d3b748e
Kaloyan Gangov <gangov1@gmail.com>
2023-01-17 00:53:21 +0200

    working multithreading

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
-use std::{env, io};
-use std::path::Path;
-use std::process::{Command};
+use std::{env, io, fs, thread};
+use std::io::{BufRead, BufReader};
+use std::path::{Path, PathBuf};
+use std::process::{Command, Stdio};
+use rayon::prelude::*;
 
-static REPOS: &'static [&str] = &["https://github.com/sqshq/piggymetrics",
+const REPOS: &'static [&str] = &["https://github.com/sqshq/piggymetrics",
     "https://github.com/spring-projects/spring-petclinic",
     "https://github.com/Yoh0xFF/java-spring-security-example",
     "https://github.com/mertakdut/Spring-Boot-Sample-Project"];
@@ -15,7 +17,6 @@ fn main() {
     let mut new_folder_name = String::new();
     io::stdin().read_line(&mut new_folder_name).expect("cannot run program");
 
-
     Command::new("mkdir")
       .current_dir(home_folder)
       .arg(new_folder_name.trim())
@@ -26,13 +27,60 @@ fn main() {
       .join(new_folder_name.trim()))
       .expect("cannot create folder");
 
-    for repo in REPOS.into_iter() {
+    REPOS
+      .par_iter()
+      .for_each(|repo| {
+          let threaded_target_dir = target_dir.clone();
+          clone_repo(&threaded_target_dir, repo)
+      });
 
-        Command::new("git")
-          .current_dir(&target_dir)
-          .arg("clone")
-          .arg(repo)
-          .output()
-          .expect("Err during cloning");
-    };
+    for entry in fs::read_dir(&target_dir).expect("cannot read dir") {
+        let entry = entry.expect("Failed to get entry");
+        let sub_dir_path = entry.path();
+
+        if sub_dir_path.is_dir() {
+            let sub_dir_path = sub_dir_path.clone();
+            let child = thread::spawn(move || {
+                let mut cmd = Command::new("mvn")
+                  .current_dir(sub_dir_path)
+                  .arg("clean")
+                  .arg("install")
+                  .stdout(Stdio::piped())
+                  .spawn()
+                  .expect("Err during mvn install");
+                let stdout = cmd.stdout.as_mut().unwrap();
+                let stdout_reader = BufReader::new(stdout);
+                let stdout_lines = stdout_reader.lines();
+                for line in stdout_lines {
+                    println!("Read: {:?}", line);
+                }
+                cmd.wait().unwrap();
+            });
+
+            child.join().expect("thread panicked");
+        }
+    }
+
+}
+
+fn clone_repo(target_dir: &PathBuf, repo: &&str) {
+    let mut cmd = Command::new("git")
+      .current_dir(&target_dir)
+      .arg("clone")
+      .arg(repo)
+      .stdout(Stdio::piped())
+      .spawn()
+      .expect("Err during cloning");
+
+    {
+        let stdout = cmd.stdout.as_mut().unwrap();
+        let stdout_reader = BufReader::new(stdout);
+        let stdout_lines = stdout_reader.lines();
+
+        for line in stdout_lines {
+            println!("Read: {:?}", line);
+        }
+    }
+
+    cmd.wait().unwrap();
 }
